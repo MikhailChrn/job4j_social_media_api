4cfb9c60aff8c9680e54aec8c9f1551ef316a0fe
MikhailChrn <mikhail.cherneyev@yandex.ru>
2025-05-06 20:07:04 +0400

    Extends spring style JPA repository using 'Repository query keywords'

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,7 +1,21 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.socialmedia.entity.Post;
+import ru.job4j.socialmedia.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    Collection<Post> findByUser(User user);
+
+    Collection<Post> findByCreatedGreaterThanEqualAndCreatedLessThanEqual(LocalDateTime startAt,
+                                                                          LocalDateTime finishAt);
+
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
+
 }
--- src/test/java/ru/job4j/socialmedia/repository/PostRepositoryTest.java
+++ src/test/java/ru/job4j/socialmedia/repository/PostRepositoryTest.java
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.job4j.socialmedia.entity.Post;
-import ru.job4j.socialmedia.entity.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
+import ru.job4j.socialmedia.entity.Post;
+import ru.job4j.socialmedia.entity.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -93,5 +97,97 @@ class PostRepositoryTest {
 
         assertEquals(postRepositoryResponse.size(), expected.size());
         assertTrue(postRepositoryResponse.containsAll(expected));
+    }
+
+    @Test
+    public void whenGetListPostsOfUserThenGetAllEntities() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+
+        User userOne = User.builder().email("One@Email").password("pass")
+                .build();
+        User userTwo = User.builder().email("Two@Email").password("pass")
+                .build();
+        List.of(userTwo, userOne)
+                .forEach(user -> userRepository.save(user));
+
+        Post post1 = Post.builder().created(now)
+                .title("example1")
+                .user(userTwo).build();
+        Post post2 = Post.builder().created(now)
+                .title("example1")
+                .user(userTwo).build();
+        Post post3 = Post.builder().created(now)
+                .title("example1")
+                .user(userOne).build();
+        Post post4 = Post.builder().created(now)
+                .title("example1")
+                .user(userTwo).build();
+        List.of(post3, post1, post4, post2)
+                .forEach(post -> postRepository.save(post));
+
+        Collection<Post> listOfUserTwo = List.of(post4, post2, post1);
+        Collection<Post> listOfUserOne = List.of(post3);
+
+        Collection<Post> postRepositoryResponseOne = (Collection<Post>)
+                postRepository.findByUser(userOne);
+        Collection<Post> postRepositoryResponseTwo = (Collection<Post>)
+                postRepository.findByUser(userTwo);
+
+        assertEquals(postRepositoryResponseOne.size(), listOfUserOne.size());
+        assertTrue(postRepositoryResponseOne.containsAll(listOfUserOne));
+        assertEquals(postRepositoryResponseTwo.size(), listOfUserTwo.size());
+        assertTrue(postRepositoryResponseTwo.containsAll(listOfUserTwo));
+    }
+
+    @Test
+    public void whenGetListPostsBetweenTwoDatesThenGetEntities() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+
+        List<LocalDateTime> dates = new ArrayList<>();
+        for (int month = 1; month <= 12; month++) {
+            dates.add(LocalDateTime.of(2024, month, 1, 0, 0));
+        }
+
+        User user = User.builder().email("One@Email")
+                .password("pass")
+                .build();
+        userRepository.save(user);
+
+        dates.forEach(date -> postRepository.save(
+                Post.builder().user(user).title("title")
+                        .created(date).build()));
+
+        Collection<Post> postRepositoryResponse = postRepository.findByCreatedGreaterThanEqualAndCreatedLessThanEqual
+                        (LocalDateTime.of(2024, 2, 15, 0, 0),
+                                LocalDateTime.of(2024, 8, 15, 0, 0));
+
+        assertEquals(postRepositoryResponse.size(), 6);
+    }
+
+    @Test
+    public void whenGetListPostsUsingPageThenGetPageWithEntities() {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+
+        List<LocalDateTime> dates = new ArrayList<>();
+        for (int month = 1; month <= 12; month++) {
+            dates.add(LocalDateTime.of(2024, month, 1, 0, 0));
+        }
+
+        User user = User.builder().email("One@Email")
+                .password("pass")
+                .build();
+        userRepository.save(user);
+
+        dates.forEach(date -> postRepository.save(
+                Post.builder().user(user).title("title")
+                        .created(date).build()));
+
+        Page<Post> postRepositoryResponse =
+                postRepository.findByOrderByCreatedDesc(PageRequest.of(2, 2));
+
+        assertEquals(postRepositoryResponse.getSize(), 2);
+
+        assertThat(postRepositoryResponse.stream().findFirst().get().getCreated())
+                .isEqualTo(LocalDateTime.of(2024, 8, 1, 0, 0));
     }
 }
